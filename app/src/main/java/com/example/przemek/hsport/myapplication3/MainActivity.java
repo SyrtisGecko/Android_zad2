a18cff8c7fcd6eefc3bbbf6fa45bdfea2aa36660
SyrtisGecko <syrtismajor@gmail.com>
2017-04-29 21:48:48 +0200

    Added Spinner and its functionality to MainActivity

--- app/src/main/java/com/example/przemek/hsport/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/przemek/hsport/myapplication3/MainActivity.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] lista = {"Position 1", "Position 2", "Position 3"};
+    String[] p = {"1", " 2", " 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner options = (Spinner) findViewById(R.id.spinner1);
+        if(options != null) {
+            options.setOnItemSelectedListener(this);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            options.setAdapter(adapter);
+        }
     }
 
     public void launchLista1(View view) {
@@ -26,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
     public void launchGrid1(View view) {
         Intent intent = new Intent(this, Grid1.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "You have chosen: " + p[position], Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
